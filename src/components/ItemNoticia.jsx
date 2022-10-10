2a1a29a46ed0eff6ee7a05c0b8ec594e8cbb3b69
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-10-10 20:31:54 -0300

    cree archivo run build

--- src/components/ItemNoticia.jsx
+++ src/components/ItemNoticia.jsx
@@ -10,7 +10,7 @@ const ItemNoticia = ({noticia}) => {
       <Card.Body>
         <Card.Title>{noticia.title}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{noticia.category}</Card.Subtitle>
-        <Card.Text className="textoRecortado">
+        <Card.Text>
           {noticia.content}
         </Card.Text>
         <a href={noticia.link}>
