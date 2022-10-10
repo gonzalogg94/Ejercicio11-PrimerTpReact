ff69b75b89e71ed046a8bb58b841c7335256fd68
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-10-10 12:46:18 -0300

    agregue url de api y realice consultas de la misma

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import { Container } from "react-bootstrap";
 import Titulo from "./components/Titulo";
 import FormularioNoticias from "./components/FormularioNoticias"
 import ListaNoticias from "./components/ListaNoticias";
+import { useEffect,useState } from "react";
 
 function App() {
+  const [noticia, setNoticia] = useState([]);
+useEffect(()=>{
+  consultarAPI();
+},[])
+
+const consultarAPI=async()=>{
+const respuesta= await fetch('https://newsdata.io/api/1/news?country=ar&apikey=pub_121255cebe5af61129044085f56a740b5796b')
+console.log(respuesta)
+}
+
+
   return (
       <Container>
         <Titulo></Titulo>
--- src/components/FormularioNoticias.jsx
+++ src/components/FormularioNoticias.jsx
@@ -7,10 +7,17 @@ const FormularioNoticias = () => {
         <Form.Group className="mb-3 d-lg-flex d-md-flex">
           <Form.Label htmlFor="disabledSelect">Buscar por categoria</Form.Label>
           <Form.Select id="disabledSelect">
-            <option>Categoria 1</option>
-            <option>Categoria 2</option>
-            <option>Categoria 3</option>
-            <option>Categoria 4</option>
+            <option>Negocios</option>
+            <option>Entretenimiento</option>
+            <option>Medio Ambiente</option>
+            <option>Gastronomia</option>
+            <option>Salud</option>
+            <option>Politica</option>
+            <option>Ciencia</option>
+            <option>Deportes</option>
+            <option>Tecnologia</option>
+            <option>Parte Superior</option>
+            <option>Mundo</option>
           </Form.Select>
         </Form.Group>
         <Button type="submit">Mostrar Noticias</Button>
